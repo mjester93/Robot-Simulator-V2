c4ba740bc3447a54530cb652ce728dfa5437de29
mjester93 <m.jester.93@gmail.com>
2020-07-15 12:57:18 -0400

    almost done

--- index.js
+++ index.js
@@ -4,21 +4,40 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	const button = document.getElementById("move-button");
 	const arrowKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
-	const movesContainer = document.getElementById("moves-container");
+  const movesContainer = document.getElementById("moves-container");
+  
+  const moveLi = (li) => {
+    let dataMove = li.getAttribute("data-move");
+    setInterval(move(dataMove), 5000);
+    li.remove();
+  }
+
+  const deleteMostRecentLi = () => {
+    let listOfLis = document.querySelectorAll("li");
+
+    if (listOfLis.length !== 0) {
+      listOfLis[listOfLis.length - 1].remove();
+    }
+  }
 	
 
 	document.addEventListener("keydown", function(e) {
-
 		if(arrowKeys.includes(e.key)){
-			let move = document.createElement("li");
-			let moveLi = e.key.replace("Arrow", "");
+      let move = document.createElement("li");
+      let moveLi = e.key.replace("Arrow", "").toLowerCase();
+      move.setAttribute('data-move', moveLi);
+      
 			move.textContent = moveLi;
 			movesContainer.appendChild(move);
-			console.log(e.key);
-		}
+    }
+    else if (e.key === "Backspace") {
+      deleteMostRecentLi();
+    }
 	});
 
 	button.addEventListener("click", function() {
-		console.log("clicked the button");
-	});
+    document.querySelectorAll("li").forEach(li => setInterval(moveLi(li), 5000))
+  });
 })
+
+
