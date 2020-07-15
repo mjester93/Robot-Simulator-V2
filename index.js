bec35e219d111d31e3d25d27362f905fc48d5b50
ianmcknnn <ianmcnnn@gmail.com>
2020-07-15 10:27:44 -0400

    added event listeners and printed directions to screen

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
-  createGrid()
-  renderBot(currentPosition)
+	createGrid()
+	renderBot(currentPosition)
 
-  // ADD CODE HERE!
+	const button = document.getElementById("move-button");
+	const arrowKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+	const movesContainer = document.getElementById("moves-container");
+	
+
+	document.addEventListener("keydown", function(e) {
+
+		if(arrowKeys.includes(e.key)){
+			let move = document.createElement("li");
+			let moveLi = e.key.replace("Arrow", "");
+			move.textContent = moveLi;
+			movesContainer.appendChild(move);
+			console.log(e.key);
+		}
+	});
+
+	button.addEventListener("click", function() {
+		console.log("clicked the button");
+	});
 })
